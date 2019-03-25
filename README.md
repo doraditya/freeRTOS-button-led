6c90f2e6b27d63a9a211815d711e0a2bd282b74a
doraditya nandanamudi <doraditya.nandanamudi@nanothingsinc.com>
2019-03-24 22:28:06 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# freeRTOS-hw
+Button LED application for free RTOS with STM32F407G-DISC1 Discovery board
+1.  
---
+++ src/main.c
@@ -0,0 +1,22 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Ac6
+  * @version V1.0
+  * @date    01-December-2013
+  * @brief   Default main function.
+  ******************************************************************************
+*/
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include "FreeRTOS.h"
+#include "stm32f4xx.h"
+#include "task.h"
+int main(void)
+{
+
+	for(;;);
+}
+
+
